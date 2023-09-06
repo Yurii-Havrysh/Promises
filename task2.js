daeaa3a0394b776cc40e83c4a685a2ebf640bb2d
Yurii Havrysh <gavrisch.yuri@gmail.com>
2023-09-06 22:12:15 +0200

    .

--- task2.js
+++ task2.js
@@ -1,10 +1,32 @@
 function promiseAllSettled(promises) {
-    return Promise.allSettled(promises)
-    .then((settlements) => settlements.forEach((settlement) => console.log(settlement.status, settlement)));
+    return new Promise((resolve) => {
+        const results = [];
+        let donePromises = 0;
+
+        for(let i = 0; i < promises.length; i++) {
+            promises[i]
+                .then((value) => {
+                results[i] = { status: 'fulfilled', value: value };
+                })
+                .catch((reason) => {
+                results[i] = { status: 'rejected', reason: reason };
+                })
+                .finally(() => {
+                donePromises++;
+      
+                if (donePromises === promises.length) {
+                  resolve(results);
+                }
+            })
+        }
+    })  
 }
 
 const promise1 = Promise.resolve(1);
-const promise2 = new Promise((resolve, reject) => setTimeout(reject, 100, 'Error occurred'));
-const promises = [promise1, promise2];
+const promise2 = Promise.reject('Error occurred');
+const promise3 = Promise.resolve(3);
 
-promiseAllSettled(promises);
+promiseAllSettled([promise1, promise2, promise3])
+    .then((results) => {
+        console.log("All promises settled:", results);
+    })
